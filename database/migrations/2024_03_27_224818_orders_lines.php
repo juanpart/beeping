fd86245f074e688482fb7b52e9a0e3070e88ea51
Juan Ignacio Part <juanipart@gmail.com>
2024-03-27 19:56:52 -0300

    creacion de migrations

---
+++ database/migrations/2024_03_27_224818_orders_lines.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders_lines', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->unsigned();
+            $table->integer('qty');
+            $table->foreignId('product_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
